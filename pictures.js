b0666c6b84f535e92d1bbb97692ded736af7b1a1
Zgbljhfc2005 <aleks007x@gmail.com>
2024-05-21 14:51:44 +0700

    Add files via upload

---
+++ pictures.js
@@ -0,0 +1,19 @@
+for (let i = 0; i < 15; i++) {
+    var card = document.getElementsByClassName("card")[i]
+    card.style.background = "url(../resources/prep"+ i + ".png) center no-repeat"
+    card.style.backgroundSize = "cover"
+}
+
+let horizontScroll=document.querySelector('.content_right');
+    let leftBtn=document.getElementById('leftBtn');
+    let rightBtn=document.getElementById('rightBtn');
+    
+    rightBtn.addEventListener('click', ()=> {
+        horizontScroll.style.scrollBehavior = 'smooth';
+        horizontScroll.scrollLeft +=1535;
+    });
+
+    leftBtn.addEventListener('click', ()=> {
+        horizontScroll.scrollLeft -=1535;    
+    });
+
