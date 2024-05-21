d482aead791ceb0279609050dbd063054f8febf2
Zgbljhfc2005 <aleks007x@gmail.com>
2024-05-21 18:12:23 +0700

    Update pictures.js

--- pictures.js
+++ pictures.js
@@ -1,6 +1,6 @@
 for (let i = 0; i < 15; i++) {
     var card = document.getElementsByClassName("card")[i]
-    card.style.background = "url(../resources/prep"+ i + ".png) center no-repeat"
+    card.style.background = "url(resources/prep"+ i + ".png) center no-repeat"
     card.style.backgroundSize = "cover"
 }
 
